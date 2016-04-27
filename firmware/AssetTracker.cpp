7e6b30af351b885b1867ced48f2c07458ba3f55c
bella32 <smathers@email.arizona.edu>
2016-04-26 20:04:55 -0700

    Update AssetTracker.cpp

--- firmware/AssetTracker.cpp
+++ firmware/AssetTracker.cpp
@@ -41,8 +41,8 @@ String AssetTracker::readLatLon(){
 void AssetTracker::gpsOn(){
     // Power to the GPS is controlled by a FET connected to D6
     pinMode(D6,OUTPUT);
-    digitalWrite(D6,LOW);
-    gps.begin(9600);
+    //digitalWrite(D6,LOW);
+    gps.begin(4800);
     gps.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
     delay(500);
     // Default is 1 Hz update rate
